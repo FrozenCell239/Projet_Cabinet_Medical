5549f96c108f1afd43a6524ebcb5a1c2bd3315f2
Oberlin V <56803555+FrozenCell239@users.noreply.github.com>
2023-03-30 16:24:56 +0200

    Update access_checker.php

--- src/access/access_checker.php
+++ src/access/access_checker.php
@@ -15,30 +15,30 @@
         enlog("Connection failed.");
     };
     if(isset($_GET['dc'])){
-        $value = $_GET['dc'];
-        $check_query = "SELECT mdp_code FROM code_visiophone WHERE mdp_code=SHA1('$value');";
+        $value = sha1($_GET['dc']);
+        $check_query = "SELECT mdp_code FROM code_visiophone WHERE mdp_code='$value';";
     };
     if(isset($_GET['rt'])){
-        $value = $_GET['rt'];
-        $check_query = "SELECT mdp_badge, actif FROM badges_visiophone WHERE mdp_badge='$value';";
+        $value = sha1($_GET['rt']);
+        $check_query = "SELECT id_badge, mdp_badge, actif FROM badges_visiophone WHERE mdp_badge='$value';";
     };
     $check_query_result = mysqli_query($conn, $check_query);
     $row = mysqli_fetch_array($check_query_result, MYSQLI_ASSOC);
     if(!isset($row['mdp_code']) && !isset($row['mdp_badge'])){
-        if(isset($_GET['rt'])){};
-        enlog("Wrong doorcode or tag !\n");
+        if(isset($_GET['rt'])){enlog($_GET['rt']." is a wrong tag !\n");};
+        if(isset($_GET['dc'])){enlog($_GET['dc']." is a wrong doorcode !\n");};
     };
     if(isset($row['mdp_badge'])){
         if($row['actif'] == 0){
-            enlog("Warning : a deactivated tag just have been used !".PHP_EOL);
+            enlog("Warning : the deactivated tag with number ".$_GET['rt']." just have been used !".PHP_EOL);
         };
         if($row['actif'] == 1){
-            enlog("Door opened with badge number ".$row['mdp_badge'].".".PHP_EOL);
+            enlog("Door opened with badge number ".$_GET['rt'].".".PHP_EOL);
             echo '$';
         };
     };
     if(isset($row['mdp_code'])){
-        enlog("Door opened with doorcode (".$row['mdp_code'].").".PHP_EOL);
+        enlog("Door opened with doorcode (".$_GET['dc'].").".PHP_EOL);
         echo '$';
     };
     unset($value);
