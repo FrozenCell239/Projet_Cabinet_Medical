fdfc06b75f373d52ce44592196a78260362b9827
Oberlin V <56803555+FrozenCell239@users.noreply.github.com>
2023-03-30 16:50:13 +0200

    Update index.php
    Replaced Bootstrap CSS with Tailwind CSS. I will do this for every pages later.

--- src/index.php
+++ src/index.php
@@ -6,11 +6,10 @@
 
         <!--Style sheets.-->
         <link rel="stylesheet" href="global.css"> <!--Customised style sheet.-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> <!--Bootstrap 5.2.3.-->
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <!--JS scripts.-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
         <!--PHP scripts.-->
         <?php
@@ -19,30 +18,24 @@
 
         <!--Others.-->
         <title>Connexion</title>
-        <link rel="icon" type="image/x-icon" href="../images/favicon.png"> <!--Favicon.-->
+        <link rel="icon" type="image/x-icon" href="favicon.png"> <!--Favicon.-->
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Permet l'adaptation de la page et la disposition de ses éléments à tous les terminaux.-->
     </head>
-    <body>
-        <header>
-            
-        </header>
-        <main>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 col-xs-12">
-                        <h1>Veuillez vous authentifier.</h1>
-                        <form action="index.php" method="post">
-                            <input type="text" name="user_login" placeholder="Identifiant" required>
-                            <input type="password" name="psswrd" placeholder="Mot de passe" required>
-                            <button type="submit" name="login">Se connecter</button>
-                        </form>
-                    </div>
+    <body class="bg-gray-200 flex justify-center items-center h-screen">
+        <div class="bg-white rounded-xl p-8 rounded shadow-md">
+            <h1 class="text-2xl font-bold mb-6">Connexion</h1>
+            <form action="index.php" method="post">
+                <div class="mb-4">
+                    <label class="block font-bold mb-2" for="user_login">Username</label>
+                    <input class="block border border-gray-400 p-2 w-full" type="text" name="user_login" id="user_login" required>
                 </div>
-            </div>
-        </main>
-        <footer>
-            
-        </footer>
+                <div class="mb-6">
+                    <label class="block font-bold mb-2" for="psswrd">Password</label>
+                    <input class="block border border-gray-400 p-2 w-full" type="password" name="psswrd" id="psswrd" required>
+                </div>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" name="login">Se connecter</button>
+            </form>
+        </div>
     </body>
 </html>
