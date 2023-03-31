a873527372fbcc3c6b05022e350b3abbd4d3f90e
Oberlin V <56803555+FrozenCell239@users.noreply.github.com>
2023-03-31 14:22:00 +0200

    Update delete.php

--- src/delete.php
+++ src/delete.php
@@ -1,7 +1,7 @@
 <?php
     include('server.php');
 
-    if(isset($_GET['id'])){
+    if(isset($_GET['what']) && $_GET['what'] == 1 && isset($_GET['id'])){
         $delete_query = "DELETE FROM personnel WHERE id_personnel=".$_GET['id'];
         mysqli_query($conn, $delete_query);
         echo 'Personnel supprimé avec succès.';
