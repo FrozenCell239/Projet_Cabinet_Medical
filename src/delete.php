3cbcddcfccf7ea7bc7e53b9635b5948b658aa831
Oberlin V <56803555+FrozenCell239@users.noreply.github.com>
2023-04-03 22:19:40 +0200

    Update delete.php
    Now handles the room and patient deletions. Also resets the GET array (this might avoid some bug).

--- src/delete.php
+++ src/delete.php
@@ -1,9 +1,27 @@
 <?php
     include('server.php');
 
+    # Staff deletion
     if(isset($_GET['what']) && $_GET['what'] == 1 && isset($_GET['id'])){
         $delete_query = "DELETE FROM personnel WHERE id_personnel=".$_GET['id'];
         mysqli_query($conn, $delete_query);
+        echo $delete_query.PHP_EOL;
         echo 'Personnel supprimé avec succès.';
     };
+
+    # Room deletion
+    if(isset($_GET['what']) && $_GET['what'] == 2 && isset($_GET['id'])){
+        $delete_query = "DELETE FROM salles WHERE id_salle=".$_GET['id'];
+        mysqli_query($conn, $delete_query);
+        echo 'Salle supprimée avec succès.';
+    };
+
+    # Patient deletion
+    if(isset($_GET['what']) && $_GET['what'] == 3 && isset($_GET['id'])){
+        $delete_query = "DELETE FROM patients WHERE id_patient=".$_GET['id'];
+        mysqli_query($conn, $delete_query);
+        echo 'Patient supprimé avec succès.';
+    };
+
+    $_GET = array();
 ?>
