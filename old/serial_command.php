edbdc39b75ceb98d36ee32fca6db994967f5b2a6
FrozenCell239 <val8ioks@gmail.com>
2023-04-19 01:54:06 +0200

    Combined both functions into an only one to save some lines and get rid of duplicate code. Also added some comments to clarify the code.

--- old/serial_command.php
+++ old/serial_command.php
@@ -1,22 +1,16 @@
 <?php
-    function strikeOpen(){ //Opens only the strike.
-        $fp = fopen("/dev/ttyACM0", "wb+");
-        if(!$fp){
-            echo "Erreur : impossible de communiquer avec la carte Arduino !";
+    /*
+        This was used to send order to Arduino board over serial cable before we get an Ethernet shield.
+        Please, note that this works only on GNU/Linux distros and not on Windows.
+    */
+    function doorControl($ORDER){
+        $fp = fopen("/dev/ttyACM0", "wb+"); //Opens the connection. //You might have to modify the path depending on your Linux distro, note that we use a Debian 11 server.
+        if(!$fp){ //If connection fails for some reason...
+            echo "Erreur : impossible de communiquer avec la carte Arduino !"; //...display an error message.
         }
         else{
-            fwrite($fp, 'A');
-            fclose($fp);
-        };
-    };
-    function doorOpen(){ //Opens both the strike and the door.
-        $fp = fopen("/dev/ttyACM0", "wb+");
-        if(!$fp){
-            echo "Erreur : impossible de communiquer avec la carte Arduino !";
-        }
-        else{
-            fwrite($fp, 'B');
-            fclose($fp);
+            fwrite($fp, $ORDER); //Sends the order to the Arduino board.
+            fclose($fp); //Closes the connection.
         };
     };
 ?>
