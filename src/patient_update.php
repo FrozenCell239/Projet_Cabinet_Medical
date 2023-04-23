da6901b2385cef285d02a846ed9e8e5a5fb1b8fe
FrozenCell239 <val8ioks@gmail.com>
2023-04-23 02:13:35 +0200

    Put the Cancel button in the first form because putting it in a second form was useless. And so it also displays correctly beside the Validate button instead being displayed below.

--- src/patient_update.php
+++ src/patient_update.php
@@ -63,8 +63,6 @@
                     />
                 </div>
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" name="patient_update">Valider</button>
-            </form>
-            <form action="patient_update.php" method="post">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" name="patient_update_cancel">Annuler</button>
             </form>
         </div>
