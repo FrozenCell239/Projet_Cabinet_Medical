ee83ab77dc32af8ff2b6c2119a8240379816f61f
FrozenCell239 <val8ioks@gmail.com>
2023-05-13 15:33:47 +0200

    Fixed the position of the delete button.

--- src/patient_update.php
+++ src/patient_update.php
@@ -122,8 +122,8 @@
                 </div>
                 <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit" name="patient_update">Valider</button>
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" onclick="location.href='patients_manage.php';" name="patient_update_cancel">Annuler</button>
-                
-            </form><button id="<?= $_SESSION['u_patient_id']; ?>" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded remove" type="submit" name="patient_delete" style="float: right;">Supprimer</button>
+                <button id="<?= $_SESSION['u_patient_id']; ?>" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded remove" type="submit" name="patient_delete" style="float: right;">Supprimer</button>
+            </form>
         </div>
     </body>
 </html>
