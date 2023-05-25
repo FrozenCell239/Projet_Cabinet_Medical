522c26c2ebc321b33e008fcd5b0fc8bcbd035d43
FrozenCell239 <val8ioks@gmail.com>
2023-05-25 17:56:51 +0200

    Some changes to match the modification of the database structure. + Removed the two useless unset(). + Minor changes.

--- src/access_checker.php
+++ src/access_checker.php
@@ -3,35 +3,29 @@
 
     if(isset($_GET['dc'])){ //Determines the SQL query for checking in case a doorcode is typed.
         $value = sha1($_GET['dc']);
-        $check_query = $conn->prepare("SELECT mdp_code FROM code_visiophone WHERE mdp_code=?;");
+        $check_query = $conn->prepare("SELECT prenom_personnel, nom_personnel FROM personnel WHERE code_porte=?;");
     };
     if(isset($_GET['rt'])){ //Determines the SQL query for checking in case a tag is detected.
         $value = base64_encode($_GET['rt']);
-        $check_query = $conn->prepare("SELECT id_badge, mdp_badge, actif FROM badges_visiophone WHERE mdp_badge=?;");
+        $check_query = $conn->prepare("SELECT prenom_personnel, nom_personnel FROM personnel WHERE numero_badge=?;");
     };
-    if(!isset($check_query)){ //Happens when no query is prepared because someone tried to access this page manually.
+    if(!isset($check_query)){ //Redirects to main page when no query is prepared because someone tried to access this page manually.
         header("Location: main.php");
     };
     $check_query->execute([$value]); //Executes the SQL query.
     $row = $check_query->fetch(); //Checking if the doorcode/tag received from Arduino exists in the database.
-    if(!isset($row['mdp_code']) && !isset($row['mdp_badge'])){ //If doorcode/tag was wrong.
-        if(isset($_GET['rt'])){enlog("Warning : ".$_GET['rt']." is a wrong tag !\n", true);}; //...then logs an error message with the tag number.
-        if(isset($_GET['dc'])){enlog("Warning : ".$_GET['dc']." is a wrong doorcode !\n", true);};  //...then logs an error message with the typed doorcode.
-    };
-    if(isset($row['mdp_badge'])){ //If the tag detected exists in the database.
-        if($row['actif'] == 0){ //If the detected tag is deactivated...
-            enlog("Warning : the deactivated tag with number ".$_GET['rt']." just have been used !".PHP_EOL, true); //...then logs an error message with the tag number.
+    if($row === false){ //If doorcode/tag was wrong...
+        if(isset($_GET['rt'])){enlog("Attention : le badge n°".$_GET['rt']." n'est pas un badge répertorié !", true);}; //...then logs an error message with the tag number.
+        if(isset($_GET['dc'])){enlog("Attention : ".$_GET['dc']." n'est pas un code de porte répertorié !", true);};  //...then logs an error message with the typed doorcode.
+    }
+    else{ //Else, the doorcode/tag exists.
+        if(isset($_GET['rt'])){ //If the tag detected exists in the database...
+            enlog("Porte devérrouilée par ".$row['prenom_personnel'].' '.$row['nom_personnel']." depuis l'extérieur avec le badge n°".$_GET['rt'].".", true); //...then logs a message with the number tag number...
         };
-        if($row['actif'] == 1){ //If the detected tag is not deactivated...
-            enlog("Door opened with badge number ".$_GET['rt'].".".PHP_EOL, true); //...then logs a message with the number tag number...
-            echo '%'; //...and send the order to open the door.
+        if(isset($_GET['dc'])){ //If the typed doorcode is right...
+            enlog("Porte devérrouilée par ".$row['prenom_personnel'].' '.$row['nom_personnel']." depuis l'extérieur avec le code de porte \"".$_GET['dc']."\".", true); //...then logs a message with the typed doorcode...
         };
-    };
-    if(isset($row['mdp_code'])){ //If the typed doorcode is right...
-        enlog("Door opened with doorcode (".$_GET['dc'].").".PHP_EOL, true); //...then logs a message with the typed doorcode...
         echo '%'; //...and send the order to open the door.
     };
-    unset($value); //Unsets the variable that contained the doorcode/tag number received from Arduino.
-    unset($check_query); //Unsets the variable that contained the SQL query.
     $conn = null; //Closes the connection to database
 ?>
