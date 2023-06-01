30963cb0ab319e6b70a608360f937ed926c22968
FrozenCell239 <val8ioks@gmail.com>
2023-06-01 09:54:01 +0200

    Now disabling and unrequiring login code and password for external staff as they're not meant to use this website. + Added the possibility to set new staff member access type and code. + Minor enhancements and changes.

--- src/app/pages/staff_add.php
+++ src/app/pages/staff_add.php
@@ -33,16 +33,6 @@
             />
         </div>
         <div class="mb-6">
-            <label class="block font-bold mb-2" for="new_staff_user_login">Identifiant</label>
-            <input
-                class="block border border-gray-400 p-2 w-full"
-                type="text"
-                name="new_staff_user_login"
-                maxlength="42"
-                required
-            />
-        </div>
-        <div class="mb-6">
             <label class="block font-bold mb-2" for="new_staff_mail">Mail</label>
             <input
                 class="block border border-gray-400 p-2 w-full"
@@ -50,6 +40,33 @@
                 name="new_staff_mail"
                 maxlength="42"
                 required
+            />
+        </div>
+        <div class="mb-6">
+            <label class="block font-bold mb-2" for="new_staff_level">Catégorie de personnel</label>
+            <select
+                class="block border border-gray-400 p-2 w-full"
+                name="new_staff_level"
+                required
+            >
+                <option value="">-- Choisir une catégorie --</option>
+                <?php
+                    foreach($privilege_levels as $key => $value){
+                        echo "<option value='$key'>$value</option>";
+                    };
+                ?>
+            </select>
+        </div>
+        <div class="mb-6">
+            <label class="block font-bold mb-2" for="new_staff_user_login">Identifiant</label>
+            <input
+                class="block border border-gray-400 p-2 w-full"
+                type="text"
+                name="new_staff_user_login"
+                maxlength="42"
+                minlength="4"
+                pattern="[a-z0-9_]+"
+                disabled
             />
         </div>
         <div class="mb-6">
@@ -61,7 +78,7 @@
                 maxlength="42"
                 pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,}"
                 title="8 caractères minimum dont au moins une majuscule, une minuscule, un chiffre, et un symbole ( @#$%^&*_=+- )."
-                required
+                disabled
             />
         </div>
         <div class="mb-6">
@@ -73,32 +90,41 @@
                 maxlength="42"
                 pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,}"
                 title="8 caractères minimum dont au moins une majuscule, une minuscule, un chiffre, et un symbole ( @#$%^&*_=+- )."
-                required
+                disabled
             />
         </div>
+        <script>
+            var
+                password_inputs = document.querySelectorAll('input[name*="password"], input[name*="login"]'),
+                staff_level = document.getElementsByName("new_staff_level")[0],
+                selected_value
+            ;
+
+            staff_level.addEventListener("change", function(){
+                selected_value = staff_level.value;
+                if(parseInt(this.value) > 0){
+                    password_inputs.forEach(function(input){
+                        input.removeAttribute("disabled");
+                        input.setAttribute("required", '');
+                    });
+                }
+                else{
+                    password_inputs.forEach(function(input){
+                        input.setAttribute("disabled", '');
+                        input.removeAttribute("required");
+                        input.value = '';
+                    });
+                };
+            });
+        </script>
         <div class="mb-6">
-            <label class="block font-bold mb-2" for="">Catégorie de personnel</label>
-            <select
-                class="block border border-gray-400 p-2 w-full"
-                name="new_staff_level"
-                required
-            >
-                <option value="">-- Choisir un médecin --</option>
-                <?php
-                    foreach($privilege_levels as $key => $value){
-                        echo "<option value='$key'>$value</option>";
-                    };
-                ?>
-            </select>
-        </div>
-        <div class="mb-6">
-            <label class="block font-bold mb-2">Code d'accès</label>
+            <label class="block font-bold mb-2" for="new_staff_access_type">Code d'accès</label>
             <label class="inline-flex items-center">
-                <input type="radio" name="new_staff_access_type" value="doorcode">
+                <input type="radio" name="new_staff_access_type" value="code_porte" required>
                 <span class="ml-2">Digicode</span>
             </label>
             <label class="inline-flex items-center ml-6">
-                <input type="radio" name="new_staff_access_type" value="tag">
+                <input type="radio" name="new_staff_access_type" value="numero_badge" required>
                 <span class="ml-2">Badge/carte</span>
             </label>
             <input
@@ -106,21 +132,18 @@
                 type="text"
                 name="new_staff_access_code"
                 required
-                disabled
             />
             <script>
                 var
                     access_type = document.getElementsByName("new_staff_access_type"),
                     code_input = document.getElementsByName("new_staff_access_code")[0],
                     doorcode_attributes = {
-                        "placeholder" : "Example 1",
                         "maxlength" : "8",
                         "pattern" : "[A-D0-9*]{4,8}",
                         "oninvalid" : "setCustomValidity('4 à 8 caractères. Les caracèters autorisés sont 0 à 9, A à D, et *.')",
                         "oninput" : "setCustomValidity('')"
                     },
                     tag_attributes = {
-                        "placeholder" : "Example 2",
                         "maxlength" : "12",
                         "pattern" : "[1-9]",
                         "oninvalid" : "setCustomValidity('12 caractères maximum allant de 1 à 9. Ne pas noter les zéros et les espaces.')",
@@ -130,8 +153,7 @@
             
                 for(var i = 0; i < access_type.length; i++){
                     access_type[i].addEventListener("change", function(){
-                        code_input.removeAttribute("disabled");
-                        if(this.value === "doorcode"){
+                        if(this.value === "code_porte"){
                             for(var attr in doorcode_attributes){
                                 code_input.setAttribute(attr, doorcode_attributes[attr]);
                             };
@@ -140,7 +162,6 @@
                             for(var attr in tag_attributes){
                                 code_input.setAttribute(attr, tag_attributes[attr]);
                             };
-                            code_input.removeAttribute("disabled");
                         };
                     });
                 };
