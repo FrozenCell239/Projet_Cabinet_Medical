398d3f84d1599cf78791776ee4f62bd1b7ffe808
FrozenCell239 <val8ioks@gmail.com>
2023-06-09 02:09:47 +0200

    Added the delete button as it was missing.

--- src/app/pages/staff_update.php
+++ src/app/pages/staff_update.php
@@ -166,6 +166,7 @@
         </div>
         <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" type="submit" name="staff_update">Valider</button>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit" onclick="location.href='staff_manage.php';" name="staff_update_cancel">Annuler</button>
+        <button id="<?= $_SESSION['u_staff_id']; ?>" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded remove" type="submit" name="staff_delete" style="float: right;">Supprimer</button>
     </form>
 </div>
 <?php include("footer.php"); ?>
