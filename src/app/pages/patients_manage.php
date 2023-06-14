5cefa59feae54e94155aa1aa47db6afc9c0ea0c5
FrozenCell239 <val8ioks@gmail.com>
2023-06-14 13:14:54 +0200

    Just removed an commented and unused modal div. I have to do a better one later.

--- src/app/pages/patients_manage.php
+++ src/app/pages/patients_manage.php
@@ -70,23 +70,6 @@
                         ?>
                     </tbody>
                 </table>
-                <!--div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Avertissement</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Êtes-vous sûr(e) de vouloir supprimer cet utilisateur ? Cette action est irréversible et n\'engage aucune autre responsabilité que la vôtre.
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                                <button type="button" class="btn btn-primary">Confirmer</button>
-                            </div>
-                        </div>
-                    </div>
-                </div-->
             </div>
         </div>
     </div>
