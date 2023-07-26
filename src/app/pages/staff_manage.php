7fba62b4fb3a1056e5220291a4bf6650952c79ce
FrozenCell239 <val8ioks@gmail.com>
2023-07-26 14:50:26 +0200

    External staff are now displayed separately from internal staff.

--- src/app/pages/staff_manage.php
+++ src/app/pages/staff_manage.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="mt-5 mb-3 d-flex justify-content-between">
-                    <h2 class="pull-left">Liste du personnel</h2>
+                    <h2 class="pull-left">Liste du personnel du cabinet</h2>
                     <button class="btn" onclick='location.href="staff_add.php";'>Ajouter</button>
                 </div>
                 <table class="table table-bordered table-striped">
@@ -21,9 +21,26 @@
                     </thead>
                     <tbody>
                         <?php
-                            $staff_list_query = $conn->prepare("SELECT id_personnel, prenom_personnel, nom_personnel, identifiant, profession, mail, niveau_privilege FROM personnel");
+                            $intern_staff_list_query = $conn->prepare("
+                                SELECT
+                                    id_personnel,
+                                    prenom_personnel,
+                                    nom_personnel,
+                                    identifiant,
+                                    profession,
+                                    mail,
+                                    niveau_privilege
+                                FROM personnel
+                                WHERE niveau_privilege > 0
+                            ;");
                             $doctor_rdv_query = $conn->prepare("
-                                SELECT id_reservation, prenom_patient, nom_patient, besoin, nom_salle, date_heure
+                                SELECT 
+                                    id_reservation,
+                                    prenom_patient,
+                                    nom_patient,
+                                    besoin,
+                                    nom_salle,
+                                    date_heure
                                 FROM reservations
                                 INNER JOIN patients
                                 ON reservations.id_patient = patients.id_patient
@@ -34,8 +51,8 @@
                                 WHERE date_heure > NOW() AND personnel.id_personnel = ?
                                 ORDER BY reservations.date_heure ASC
                             ;");
-                            $staff_list_query->execute();
-                            while($row = $staff_list_query->fetch()){
+                            $intern_staff_list_query->execute();
+                            while($row = $intern_staff_list_query->fetch()){
                                 echo(
                                     '<tr id="'.$row['id_personnel'].'">'.
                                     "<td>".$row['prenom_personnel']."</td>".
@@ -64,6 +81,52 @@
                                     );
                                 };
                                 echo "</td></tr>";
+                            };
+                            unset($row);
+                            unset($rdv_row);
+                        ?>
+                    </tbody>
+                </table>
+                <div class="mt-5 mb-3 d-flex justify-content-between">
+                    <h2 class="pull-left">Liste du personnel externe</h2>
+                    <button class="btn" onclick='location.href="staff_add.php";'>Ajouter</button>
+                </div>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>Profession</th>
+                            <th>Mail</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $extern_staff_list_query = $conn->prepare("
+                                SELECT
+                                    id_personnel,
+                                    prenom_personnel,
+                                    nom_personnel,
+                                    identifiant,
+                                    profession,
+                                    mail
+                                FROM personnel
+                                WHERE niveau_privilege = 0
+                            ;");
+                            $extern_staff_list_query->execute();
+                            while($row = $extern_staff_list_query->fetch()){
+                                echo(
+                                    '<tr id="'.$row['id_personnel'].'">'.
+                                    "<td>".$row['prenom_personnel']."</td>".
+                                    "<td>".$row['nom_personnel']."</td>".
+                                    "<td>".ucfirst($row['profession'])."</td>".
+                                    "<td>".$row['mail']."</td>".
+                                    "<td>".
+                                    '<a class="btn btn-info btn-sm" href="staff_update.php?stfid_u='.$row['id_personnel'].'">Modifier</a>'.
+                                    "</td>".
+                                    "</tr>"
+                                );
                             };
                             unset($row);
                             unset($rdv_row);
